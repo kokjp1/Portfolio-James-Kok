a7968ac7fed277fe0c586ea88a1c14dddc5ccfaa
kokjp1 <james.kok@hva.nl>
2025-04-07 21:56:51 +0200

    changed to docs instead of dist

--- docs/Portfolio-James-Kok.91b5bbd7.js
+++ docs/Portfolio-James-Kok.91b5bbd7.js
@@ -160,7 +160,7 @@
       });
     }
   }
-})({"9JJ5D":[function(require,module,exports,__globalThis) {
+})({"1TsLK":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
@@ -168,7 +168,7 @@ var HMR_SERVER_PORT = 1234;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
-module.bundle.HMR_BUNDLE_ID = "7055c94b59712999";
+module.bundle.HMR_BUNDLE_ID = "3336dfa191b5bbd7";
 "use strict";
 /* global HMR_HOST, HMR_PORT, HMR_SERVER_PORT, HMR_ENV_HASH, HMR_SECURE, HMR_USE_SSE, chrome, browser, __parcel__import__, __parcel__importScripts__, ServiceWorkerGlobalScope */ /*::
 import type {
@@ -668,6 +668,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"4M6V8":[function(require,module,exports,__globalThis) {
 
-},{}]},["9JJ5D","4M6V8"], "4M6V8", "parcelRequireb634", {})
+},{}]},["1TsLK","4M6V8"], "4M6V8", "parcelRequireb634", {})
 
-//# sourceMappingURL=Portfolio-James-Kok.59712999.js.map
+//# sourceMappingURL=Portfolio-James-Kok.91b5bbd7.js.map
